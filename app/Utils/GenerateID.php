aa823da88a414760e912f55a5a8c1c76f61a96da
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-09-13 02:00:49 +0530

    generate ID
    accordingly create ids

--- app/Utils/GenerateID.php
+++ app/Utils/GenerateID.php
@@ -1,1 +1,32 @@
 <!-- function to create id accordingly -->
+<?php
+
+namespace App\Utils;
+
+use Illuminate\Support\Facades\DB;
+
+class IDCreator {
+
+    public static function createId ($prefix) {
+
+        $table = $prefix === 'DC' ? 'doctor' :
+            ($prefix === 'PT' ? 'patient' :
+            ($prefix === 'PR' ? 'prescription' :
+            ($prefix === 'MD' ? 'drug' :
+            ($prefix === 'PY' ? 'payment' :
+            ($prefix === 'AP' ? 'appointment' : 'unknown')))));
+
+        $maxId = DB::table($table) -> max('id');
+
+        if ($maxId) {
+            $updated_value = (int)substr($maxId, strlen($prefix)) + 1 ;
+            $new_id = $prefix . str_pad($updated_value, 3, '0', STR_PAD_LEFT);
+        } else {
+            $new_id = $prefix . '001';
+        }
+
+        return $new_id;
+
+    }
+
+}
